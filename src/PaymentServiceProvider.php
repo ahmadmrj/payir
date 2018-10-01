1d072f7f241c8816691f53c5b7162866f1d82201
ahmadmrj <ahmad.mrj@gmail.com>
2018-10-01 12:03:29 +0330

    Update PaymentServiceProvider.php

--- src/PaymentServiceProvider.php
+++ src/PaymentServiceProvider.php
@@ -1,9 +1,19 @@
 <?php
 
-namespace Ahmadmrj/Payir;
+namespace Ahmadmrj\Payir;
 
 use Illuminate\Support\ServiceProvider;
 
 class PaymentServiceProvider extends ServiceProvider{
-
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('Payment', function ($app) {
+            return new Payment;
+        });
+    }
 }
